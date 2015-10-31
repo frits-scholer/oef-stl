885f91aedc1a8d23247467880532330f8ae2ca5b
frits-scholer <frits.scholer@gmail.com>
2015-10-31 14:19:11 +0100

    in ex03 extra pair of () to construct vector

--- README.md
+++ README.md
@@ -10,3 +10,8 @@ ex02.cpp
 
 ex. 7. Using remove_if and a custom callback function, write a function RemoveShortWords that
 accepts a vector<string> and removes all strings of length 3 or less from it.
+
+ex03.cpp
+
+ex. 8. Calculate distance of a point in space to origin.  Write a function DistanceToOrigin that
+accepts a vector<double> and returns the distance to the origin. Use inner_product.
---
+++ ex03.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <numeric>
+#include <sstream>
+#include <string>
+#include <iterator>
+using namespace std;
+
+double DistanceToOrigin(const vector<double>& p) {
+  return sqrt(inner_product(p.begin(), p.end(), p.begin(), 0.0));
+}
+
+int main() {
+  cout << "Enter space separated point coordinates:\n";
+  string pointstr;
+  getline(cin, pointstr);
+  istringstream nrstream(pointstr);
+  //istream_iterator<double> ii(nrstream);
+  //istream_iterator<double> eos;
+  vector<double> v((istream_iterator<double>(nrstream)), istream_iterator<double>());
+  cout << "Distance to origin: " << DistanceToOrigin(v) << endl;
+}
